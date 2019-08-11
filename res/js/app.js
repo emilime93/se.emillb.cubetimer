d17af9421bf64f17b87d13fa94af9da6c8dfc2ac
Emil <emil93@gmail.com>
2019-08-11 20:23:09 +0200

    Reindented and made minor changes to listeners in app.js

--- res/js/app.js
+++ res/js/app.js
@@ -1,21 +1,44 @@
 const ui = new UI();
 const timer = new Timer(10);
 ui.displayTime(new Date(0));
-document.body.addEventListener('keyup', (e) => {
-    e.preventDefault();
-    if (!(e.key === ' '))
-        return;
-    if(timer.isTimerGoing()) {
-        mainStop();
-    } else {
-        mainStart();
-    }
+document.body.addEventListener('keyup', (event) => {
+  event.preventDefault();
+  if (!(event.key === ' '))
+  return;
+  if(timer.isTimerGoing()) {
+    mainStop();
+  } else {
+    mainStart();
+  }
 });
 
+const NOT_STARTED = 'NOT_STARTED';
+const STARTED = 'STARTED';
+const TOUCH_STOPPED = 'TOUCH_STOPPED';
+
+let touchScenario = NOT_STARTED;
+ui.display.addEventListener('touchstart', (event) => {
+  if(timer.isTimerGoing() && touchScenario !== NOT_STARTED) {
+    mainStop();
+    touchScenario = TOUCH_STOPPED;
+  }
+});
+ui.display.addEventListener('touchend', (event) => {
+  if(!timer.isTimerGoing()) {
+    if (touchScenario === NOT_STARTED) {
+      mainStart();
+      touchScenario = STARTED;
+    } else if (touchScenario === TOUCH_STOPPED) {
+      touchScenario = NOT_STARTED;
+    }
+  }
+});
+
+
 function puzzleChange(e) {
-    console.log("Select event:", e);
-    e.target.blur();
-    document.body.focus();
+  console.log("Select event:", e);
+  e.target.blur();
+  document.body.focus();
 }
 const select = document.getElementById('puzzle-select');
 select.addEventListener('change', puzzleChange);
@@ -24,27 +47,27 @@ select.addEventListener('keydown', puzzleChange);
 document.getElementById('time-table').addEventListener('click', ui.deleteTime);
 
 window.addEventListener('keydown', (e) => {
-    if (e.key === ' ' && e.target === document.body) {
-        e.preventDefault(); // Prevent space scrolling :)
-        // document.body.focus();
-    }
+  if (e.key === ' ' && e.target === document.body) {
+    e.preventDefault(); // Prevent space scrolling :)
+    // document.body.focus();
+  }
 });
 
 function mainStart() {
-    timer.start((time) => {
-        ui.displayTime(time);
-    });
+  timer.start((time) => {
+    ui.displayTime(time);
+  });
 }
 
 function mainStop() {
-    const result = timer.stop();
-    ui.addTimeToTable(result);
-    Storage.saveTime(result);
+  const result = timer.stop();
+  ui.addTimeToTable(result);
+  Storage.saveTime(result);
 }
 
 if (localStorage.length > 0) {
-    const times = Storage.loadAllTimes();
-    for (let key in times) {
-        ui.addTimeToTable(times[key]);
-    }
+  const times = Storage.loadAllTimes();
+  for (let key in times) {
+    ui.addTimeToTable(times[key]);
+  }
 }
--- res/js/cubetimer.js
+++ res/js/cubetimer.js
@@ -1,242 +1,256 @@
 class Timer {
-
-    /**
-     * The real time accuracy of the displayed time. How often it should check
-     * @param {Number} interval Interval real time accuracy
-     */
-    constructor(interval) {
-        this.interval = interval;
-        this.totalTime = 0;
-        this.timerGoing = false;
+  
+  /**
+  * The real time accuracy of the displayed time. How often it should check
+  * @param {Number} interval Interval real time accuracy
+  */
+  constructor(interval) {
+    this.interval = interval;
+    this.totalTime = 0;
+    this.timerGoing = false;
+  }
+  
+  /**
+  * @Returns wether the timer is going or not
+  */
+  isTimerGoing() {
+    return this.timerGoing;
+  }
+  
+  /**
+  * @param {funciton} callback Callback function for updating UI with time
+  */
+  start(callback) {
+    if (this.timerGoing) {
+      console.log('timer already going');
+      return;
     }
-
-    /**
-     * Returns wether the timer is going or not
-     */
-    isTimerGoing() {
-        return this.timerGoing;
-    }
-
-    /**
-     * @param {funciton} callback Callback function for updating UI with time
-     */
-    start(callback) {
-        if (this.timerGoing) {
-            console.log('timer already going');
-            return;
-        }
-        this.callback = callback;
-        this.timerGoing = true;
-        this.lastTime = new Date();
-        this.timer = setInterval(() => {
-            const delta = Date.now() - this.lastTime;
-            if (delta >= this.interval) {
-                this.totalTime += delta;
-                this.lastTime = Date.now();
-                callback(new Date(this.totalTime));
-            }
-        }, 10);
-    }
+    this.callback = callback;
+    this.timerGoing = true;
+    this.lastTime = new Date();
+    this.timer = setInterval(() => {
+      const delta = Date.now() - this.lastTime;
+      if (delta >= this.interval) {
+        this.totalTime += delta;
+        this.lastTime = Date.now();
+        callback(new Date(this.totalTime));
+      }
+    }, 10);
+  }
+  
+  stop() {
+    // Cleanup
+    this.timerGoing = false;
+    this.totalTime += Date.now() - this.lastTime;
+    this.callback(new Date(this.totalTime));
+    clearInterval(this.timer);
     
-    stop() {
-        // Cleanup
-        this.timerGoing = false;
-        this.totalTime += Date.now() - this.lastTime;
-        this.callback(new Date(this.totalTime));
-        clearInterval(this.timer);
-
-        // Build return object
-        const date = new Date(this.totalTime);
-        const stoppedTime = new Timestamp(date.getMilliseconds(), date.getSeconds(), date.getMinutes());
-        const type = ui.cubeSelect.value;
-        this.totalTime = 0;
-        return new CubeTime(Storage.getCurrentID(), type, stoppedTime);
-    }
-
-    reset() {
-        this.stop();
-        this.totalTime = 0;
-    }
+    // Build return object
+    const date = new Date(this.totalTime);
+    const stoppedTime = new Timestamp(date.getMilliseconds(), date.getSeconds(), date.getMinutes());
+    const type = ui.cubeSelect.value;
+    this.totalTime = 0;
+    return new CubeTime(Storage.getCurrentID(), type, stoppedTime);
+  }
+  
+  reset() {
+    this.stop();
+    this.totalTime = 0;
+  }
 }
 
 class UI {
-
-    constructor() {
-        this.display = document.getElementById('display');
-        this.table = document.getElementById('time-table');
-        
-        // Remove?
-        this.cubeSelect = document.getElementById('puzzle-select');
+  
+  constructor() {
+    this.display = document.getElementById('display');
+    this.table = document.getElementById('time-table');
+    
+    // Remove?
+    this.cubeSelect = document.getElementById('puzzle-select');
+  }
+  
+  /**
+  * Formats a timestamp in Date form correctly
+  * @param {Date | Timestamp} date 
+  */
+  formatTime(date) {
+    if (date instanceof Timestamp) {
+      let formattedString = '';
+      if (date.min) {
+        formattedString += `${"00".substr(date.min.toString().length) + date.min}:`;
+      }
+      formattedString += `${"00".substr(date.sec.toString().length) + date.sec}:`;
+      formattedString += `${"00".substr(date.millis.toString().length) + date.millis.toString().substr(0,2)}`;
+      return formattedString;
+    } else if (date instanceof Date) {
+      const timeStr = date.toISOString().substr(11, 11);
+      if (Number.parseInt(timeStr.substr(0, 2)) > 0) {
+        return timeStr;
+      } else if (Number.parseInt(timeStr.substr(3, 2)) > 0) {
+        return timeStr.substr(3);
+      } else {
+        return timeStr.substr(6);
+      }
+    } else {
+      console.error("it was nothing");
     }
-
-    /**
-     * Formats a timestamp in Date form correctly
-     * @param {Date | Timestamp} date 
-     */
-    formatTime(date) {
-        if (date instanceof Timestamp) {
-            let formattedString = '';
-            if (date.min) {
-                formattedString += `${"00".substr(date.min.toString().length) + date.min}:`;
-            }
-            formattedString += `${"00".substr(date.sec.toString().length) + date.sec}:`;
-            formattedString += `${"00".substr(date.millis.toString().length) + date.millis.toString().substr(0,2)}`;
-            return formattedString;
-        } else if (date instanceof Date) {
-            const timeStr = date.toISOString().substr(11, 11);
-            if (Number.parseInt(timeStr.substr(0, 2)) > 0) {
-                return timeStr;
-            } else if (Number.parseInt(timeStr.substr(3, 2)) > 0) {
-                return timeStr.substr(3);
-            } else {
-                return timeStr.substr(6);
-            }
-        } else {
-            console.error("it was nothing");
-        }
-    }
-
-    /**
-     * Displays the timestamp in the UI.
-     * @param {Date} date A time in date form to display
-     */
-    displayTime(date) { // A date object
-        this.display.textContent = this.formatTime(date);
-    }
-
-    /**
-     * 
-     * @param {CubeTime} cubeTime The time taken
-     */
-    addTimeToTable(cubeTime) {
-        const row = document.createElement('tr');
-        const idCell = document.createElement('td');
-        idCell.textContent = cubeTime.id;
-        row.appendChild(idCell);
-        const timeCell = document.createElement('td');
-        timeCell.textContent = this.formatTime(cubeTime.timestamp);
-        row.appendChild(timeCell);
-        const typeCell = document.createElement('td');
-        typeCell.textContent = cubeTime.puzzleType;
-        row.appendChild(typeCell);
-        const completeCell = document.createElement('td');
-        completeCell.textContent = cubeTime.dnf;
-        row.appendChild(completeCell);
-        this.table.appendChild(row);
-        const removeCell = document.createElement('td');
-        const removeButton = document.createElement('a');
-        removeButton.textContent = 'X';
-        removeButton.className = 'delete-time';
-        removeButton.href = '#';
-        removeCell.appendChild(removeButton);
-        row.appendChild(removeCell);
-    }
-
-    deleteTime(e) {
-        e.preventDefault();
-        if (e.target.className !== "delete-time")
-            return;
-        Storage.deleteTime(e.path[2].children[0].textContent);
-        this.removeChild(e.path[2]);
-    }
+  }
+  
+  /**
+  * Displays the timestamp in the UI.
+  * @param {Date} date A time in date form to display
+  */
+  displayTime(date) { // A date object
+    this.display.textContent = this.formatTime(date);
+  }
+  
+  /**
+  * 
+  * @param {CubeTime} cubeTime The time taken
+  */
+  addTimeToTable(cubeTime) {
+    const row = document.createElement('tr');
+    const idCell = document.createElement('td');
+    idCell.textContent = cubeTime.id;
+    row.appendChild(idCell);
+    const timeCell = document.createElement('td');
+    timeCell.textContent = this.formatTime(cubeTime.timestamp);
+    row.appendChild(timeCell);
+    const typeCell = document.createElement('td');
+    typeCell.textContent = cubeTime.puzzleType;
+    row.appendChild(typeCell);
+    const completeCell = document.createElement('td');
+    completeCell.textContent = cubeTime.dnf;
+    row.appendChild(completeCell);
+    this.table.appendChild(row);
+    const removeCell = document.createElement('td');
+    const removeButton = document.createElement('a');
+    removeButton.textContent = 'X';
+    removeButton.className = 'delete-time';
+    removeButton.href = '#';
+    removeCell.appendChild(removeButton);
+    row.appendChild(removeCell);
+  }
+  
+  deleteTime(e) {
+    e.preventDefault();
+    if (e.target.className !== "delete-time")
+    return;
+    Storage.deleteTime(e.path[2].children[0].textContent);
+    this.removeChild(e.path[2]);
+  }
 }
 
 class Storage {
-
-    /**
-     * Gets the current ID
-     * !!! AND !!! increments it
-     */
-    static getCurrentID() {
-        if (!localStorage.getItem("id")) {
-            localStorage.setItem("id", 0);
-        }
-        const oldID = Number.parseInt(localStorage.getItem("id"))
-        localStorage.setItem("id", Number.parseInt(localStorage.getItem("id")) + 1);
-        return oldID;
+  
+  /**
+  * Gets the current ID
+  * !!! AND !!! increments it
+  */
+  static getCurrentID() {
+    if (!localStorage.getItem("id")) {
+      localStorage.setItem("id", 0);
     }
-
-    /**
-     * @param {CubeTime} cubeTime A CubeTime object
-     */
-    static saveTime(cubeTime) { // A date object
-        localStorage.setItem(cubeTime.id, JSON.stringify(cubeTime));
+    const oldID = Number.parseInt(localStorage.getItem("id"))
+    localStorage.setItem("id", Number.parseInt(localStorage.getItem("id")) + 1);
+    return oldID;
+  }
+  
+  /**
+  * @param {CubeTime} cubeTime A CubeTime object
+  */
+  static saveTime(cubeTime) { // A date object
+    localStorage.setItem(cubeTime.id, JSON.stringify(cubeTime));
+  }
+  
+  /**
+  * Deletes a stored solve/time
+  * @param {Number} id The id of the solve to delete
+  */
+  static deleteTime(id) {
+    localStorage.removeItem(id);
+  }
+  
+  /**
+  * Loads an array of CubeTimes from localstorage
+  */
+  static loadAllTimes() {
+    let timeArray = [];
+    for (let key in {...localStorage}) {
+      if (key == "id") continue;
+      const parsed = new CubeTime(JSON.parse(localStorage.getItem(key)));
+      timeArray.push(parsed);
     }
+    console.log(timeArray);
+    return timeArray;
+  }
 
-    /**
-     * Deletes a stored solve/time
-     * @param {Number} id The id of the solve to delete
-     */
-    static deleteTime(id) {
-        localStorage.removeItem(id);
-    }
+  /**
+   * @returns The JSON object with preference:value for the preferences
+   */
+  static loadPreferences() {
+    return JSON.parse(localStorage.getItem('preferences'));
+  }
 
-    /**
-     * Loads an array of CubeTimes from localstorage
-     */
-    static loadAllTimes() {
-        let timeArray = [];
-        for (let key in {...localStorage}) {
-            if (key == "id")
-                continue;
-            const parsed = new CubeTime(JSON.parse(localStorage.getItem(key)));
-            timeArray.push(parsed);
-        }
-        console.log(timeArray);
-        return timeArray;
-    }
-
-    /**
-     * Loads a signle CubeTime, typically for modification before saving it back
-     * @param {Number} id The ID of the CubeTime to load
-     */
-    static loadTime(id) {
-        return new CubeTime(JSON.parse(localStorage.getItem(id)));
-    }
+  /**
+   * Saves the user preferences to the localstorage
+   * @param {Object} prefs The JSON object of preference:value members
+   */
+  static savePreferences(prefs) {
+    localStorage.setItem('preferences', JSON.stringify(prefs));
+  }
+  
+  /**
+  * Loads a signle CubeTime, typically for modification before saving it back
+  * @param {Number} id The ID of the CubeTime to load
+  */
+  static loadTime(id) {
+    return new CubeTime(JSON.parse(localStorage.getItem(id)));
+  }
 }
 
 class Timestamp {
-    /**
-     * @param {Number} millis The amount of milliseconds taken
-     * @param {Number} sec The amount of seconds taken
-     * @param {Number} min The amounds of minutes taken
-     */
-    constructor(millis, sec, min) {
-        if (typeof(millis) !== 'number') {
-            const oldObj = millis;
-            this.millis = oldObj.millis;
-            this.sec = oldObj.sec;
-            this.min = oldObj.min;
-        } else {
-            this.millis = millis;
-            this.sec = sec;
-            this.min = min;
-        }
+  /**
+  * @param {Number} millis The amount of milliseconds taken
+  * @param {Number} sec The amount of seconds taken
+  * @param {Number} min The amounds of minutes taken
+  */
+  constructor(millis, sec, min) {
+    if (typeof(millis) !== 'number') {
+      const oldObj = millis;
+      this.millis = oldObj.millis;
+      this.sec = oldObj.sec;
+      this.min = oldObj.min;
+    } else {
+      this.millis = millis;
+      this.sec = sec;
+      this.min = min;
     }
+  }
 }
 
 class CubeTime {
-    /**
-     * @param {Number | CubeTime} id Unique ID for solve
-     * @param {String} puzzleType The puzzle solved
-     * @param {Timestamp} timestamp Timestamp for the solve
-     * @param {Boolean} dnf Wether the solve is DNF, false default
-     */
-    constructor(id, puzzleType, timestamp, dnf = false, sessionID = 'DEFAULT') {
-        // For recreating a object from a serialized obj
-        if (typeof(id) !== 'number') {
-            const oldObj = id;
-            this.id = oldObj.id;
-            this.puzzleType = oldObj.puzzleType;
-            this.dnf = oldObj.dnf;
-            this.timestamp = new Timestamp(oldObj.timestamp);
-            this.sessionID = oldObj.sessionID;
-        } else {
-            this.id = id;
-            this.puzzleType = puzzleType;
-            this.timestamp = timestamp;
-            this.dnf = dnf;
-            this.sessionID = sessionID;
-        }
+  /**
+  * @param {Number | CubeTime} id Unique ID for solve
+  * @param {String} puzzleType The puzzle solved
+  * @param {Timestamp} timestamp Timestamp for the solve
+  * @param {Boolean} dnf Wether the solve is DNF, false default
+  */
+  constructor(id, puzzleType, timestamp, dnf = false, sessionID = 'DEFAULT') {
+    // For recreating a object from a serialized obj
+    if (typeof(id) !== 'number') {
+      const oldObj = id;
+      this.id = oldObj.id;
+      this.puzzleType = oldObj.puzzleType;
+      this.dnf = oldObj.dnf;
+      this.timestamp = new Timestamp(oldObj.timestamp);
+      this.sessionID = oldObj.sessionID;
+    } else {
+      this.id = id;
+      this.puzzleType = puzzleType;
+      this.timestamp = timestamp;
+      this.dnf = dnf;
+      this.sessionID = sessionID;
     }
+  }
 }
